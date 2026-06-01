33fd1bc7c94f1e25c6ae2e00e7cdde68bfe79c54
rpandya <rajesh.pandya@gmail.com>
2026-06-01 00:28:39 -0700

    Update emotion_detection.py

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -20,29 +20,40 @@ def emotion_detector(text_to_analyze):
     #print(formatted_response['emotionPredictions'][0]['emotionMentions'][0]['span']['text'])
     #return(formatted_response['emotionPredictions'][0]['emotionMentions'][0]['span']['text'])
 
-    anger = formatted_response['emotionPredictions'][0]['emotionMentions'][0]['emotion']['anger']
-    fear = formatted_response['emotionPredictions'][0]['emotionMentions'][0]['emotion']['fear']
-    disgust = formatted_response['emotionPredictions'][0]['emotionMentions'][0]['emotion']['disgust']
-    joy = formatted_response['emotionPredictions'][0]['emotionMentions'][0]['emotion']['joy']
-    sadness = formatted_response['emotionPredictions'][0]['emotionMentions'][0]['emotion']['sadness']
-    
-    emotions_dict = {
-    'anger': anger,
-    'disgust': disgust,
-    'fear': fear,
-    'joy': joy,
-    'sadness': sadness
-    }
+    if response.status_code == 400:
+        emotions_dict = {
+        'anger':None,
+        'disgust':None,
+        'fear':None,
+        'joy':None,
+        'sadness':None,
+        'dominant_emotion':None
+        }
+    else:
+        anger = formatted_response['emotionPredictions'][0]['emotionMentions'][0]['emotion']['anger']
+        fear = formatted_response['emotionPredictions'][0]['emotionMentions'][0]['emotion']['fear']
+        disgust = formatted_response['emotionPredictions'][0]['emotionMentions'][0]['emotion']['disgust']
+        joy = formatted_response['emotionPredictions'][0]['emotionMentions'][0]['emotion']['joy']
+        sadness = formatted_response['emotionPredictions'][0]['emotionMentions'][0]['emotion']['sadness']
 
-    # Find the key with the highest value
-    dominant_emotion = max(emotions_dict, key=emotions_dict.get)
-    emotions_dict = {
-    'anger': anger,
-    'disgust': disgust,
-    'fear': fear,
-    'joy': joy,
-    'sadness': sadness,
-    'dominant_emotion':dominant_emotion
-    }
+        emotions_dict = {
+        'anger': anger,
+        'disgust': disgust,
+        'fear': fear,
+        'joy': joy,
+        'sadness': sadness
+        }
+
+        # Find the key with the highest value
+        dominant_emotion = max(emotions_dict, key=emotions_dict.get)
+        emotions_dict = {
+        'anger': anger,
+        'disgust': disgust,
+        'fear': fear,
+        'joy': joy,
+        'sadness': sadness,
+        'dominant_emotion':dominant_emotion
+        }
     print(emotions_dict)
     return(emotions_dict)
+
